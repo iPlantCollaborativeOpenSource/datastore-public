f7a55d99ecf1f9ab46596755e04b2c1b3cba9a2c
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-03-05 15:58:33 -0700

    Added home view

--- sra/views.py
+++ sra/views.py
@@ -9,13 +9,22 @@ from .models import (
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'sra'}
+#@view_config(route_name='home', renderer='templates/mytemplate.pt')
+#def my_view(request):
+#    try:
+#        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+#    except DBAPIError:
+#        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+#    return {'one': one, 'project': 'sra'}
+
+@view_config(route_name='home', renderer='templates/home.pt')
+def my_vew(request):
+    return {
+        'base_url' : '123', 
+        'root': '/',
+        'dir': 'hello',
+        'year': '2013'
+    }
 
 conn_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
