af8f714cec73c854dc0e52c88b989fe9ae1d3035
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-03-10 13:05:54 -0700

    Added DSMetadata multidict class

--- sra/datastore/DSFile.py
+++ sra/datastore/DSFile.py
@@ -1,15 +1,17 @@
 from irods import *
 from os.path import basename
+from DSMetadata import DSMetadata
 
 class DSFile(object):
     def __init__(self, conn, path):
         self._conn = conn
+        self._meta = None
 
-    @staticmethod
-    def dictify_metadata(metadata):
-        def unpack_tuple(t):
-            return (t[0], (t[1], t[2]))
-        return dict(map(unpack_tuple, metadata))
+    @property
+    def metadata(self):
+        if not self._meta:
+            self._meta = DSMetadata(self._file)
+        return self._meta
 
 class DSDataObject(DSFile):
     def __init__(self, conn, path):
@@ -17,9 +19,6 @@ class DSDataObject(DSFile):
         self._file = iRodsOpen(conn, path, 'r')
         self.name = self._file.getDataName()
         self.path = self._file.getPath() + "/" + self.name
-
-    def get_metadata(self):
-        return DSFile.dictify_metadata(self._file.getUserMetadata())
 
     def __iter__(self):
         return DSDataObjectIter(self._conn, self.path)
@@ -41,17 +40,14 @@ class DSDataObjectIter(object):
 class DSCollection(DSFile):
     def __init__(self, conn, path):
         DSFile.__init__(self, conn, path)
-        self._collection = irodsCollection(conn, path)
-        self.path = self._collection.getCollName()
+        self._file = irodsCollection(conn, path)
+        self.path = self._file.getCollName()
         self.name = basename(self.path)
 
     def get_subcollections(self):
-        subcollections = self._collection.getSubCollections()
+        subcollections = self._file.getSubCollections()
         return map(lambda dir_name: DSCollection(self._conn, self.path + "/" + dir_name), subcollections)
 
     def get_objects(self):
-        objects = self._collection.getObjects()
+        objects = self._file.getObjects()
         return map(lambda obj: DSDataObject(self._conn, self.path + "/" + obj[0]), objects)
-
-    def get_metadata(self):
-        return DSFile.dictify_metadata(self._collection.getUserMetadata())
