a3c97c5b2377750c2e1c30635e0f83559a9b33ce
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-03-09 17:26:13 -0700

    Basic sidebar displays study names

--- sra/static/js/app.js
+++ sra/static/js/app.js
@@ -1,4 +1,4 @@
-var App = {Models: {}, Collections: {}};
+var App = {Models: {}, Collections: {}, Views: {}};
 
 App.Models.Study = Backbone.Model.extend();
 
@@ -7,16 +7,39 @@ App.Collections.StudyCollection = Backbone.Collection.extend({
     url: "api/study"
 });
 
+App.Views.StudyList = Backbone.View.extend({
+    initialize: function(options) {
+        console.log(options);
+        App.studies.bind('reset', this.add_studies, this);
+    },
+    render: function() {
+        this.$el.html("HELLO WORLD");
+    },
+    add_studies: function() {
+        this.$el.empty();
+        $list = $('<ul>', {id: 'study-list'});
+        App.studies.each(function(model) {
+            $('<li>').append(
+                $('<a>', {href: '#'}).append(model.get('name'))
+            ).appendTo($list); 
+        });
+        this.$el.append($list);
+    }
+});
+
 App.Router = Backbone.Router.extend({
     routes: {
         "" : "list"
     },
     list: function() {
         App.studies = new App.Collections.StudyCollection();
+        new App.Views.StudyList({el: $('#sidebar')[0]}).render();
         App.studies.fetch();
         console.log(App.studies);
     }
 });
 
-var app = new App.Router();
-Backbone.history.start();
+$(document).ready(function() {
+    var app = new App.Router();
+    Backbone.history.start();
+});
