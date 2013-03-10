48e85c8a1bcc360220fd1f069410387d89816ac4
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-03-09 18:43:21 -0700

    Added metadata to irods collection result

--- sra/datastore/__init__.py
+++ sra/datastore/__init__.py
@@ -2,6 +2,9 @@ from irods import *
 
 
 class DataStore(object):
+    def __init__(self):
+        self.conn = None
+
     def configure(self, host=None, port=None, zone=None, path=None, user=None, password=None):
         self.host = host
         self.port = port
@@ -11,16 +14,30 @@ class DataStore(object):
         self.password = password
 
     def _connect(self):
-        conn, err = rcConnect(self.host, self.port, self.user, self.zone)
-        status = clientLoginWithPassword(conn, self.password)
-        return conn
+        if not self.conn:
+            conn, err = rcConnect(self.host, self.port, self.user, self.zone)
+            status = clientLoginWithPassword(conn, self.password)
+            self.conn = conn
+        return self.conn
 
     def get_studies(self):
         conn = self._connect()
         c = irodsCollection(conn, self.path)
         base_path = c.getCollName()
 
-        studies = map(lambda dir_name: {"id": dir_name, "path" : base_path + "/" + dir_name}, c.getSubCollections())
+        studies = map(lambda dir_name: {
+            "id": dir_name, 
+            "path" : base_path + "/" + dir_name,
+            "metadata" : self.get_collection_metadata(base_path + "/" + dir_name)
+        }, c.getSubCollections())
         return studies
+
+    def get_collection_metadata(self, path):
+        conn = self._connect()
+        c = irodsCollection(conn, path)
+        def unpack_tuple(t):
+            return (t[0], (t[1], t[2]))
+        return dict(map(unpack_tuple, c.getUserMetadata()))
+        
 
 DataStoreSession = DataStore()
