5771e62d5fd41ee5e89fc7429201629f63d611da
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-05-30 14:25:07 -0700

    Added 'no preview availble' view

--- sra/static/js/datastore.js
+++ sra/static/js/datastore.js
@@ -112,13 +112,9 @@ Datastore.Views.FileView = Backbone.View.extend({
     render: function() {
         this.$el.append(new Datastore.Views.DataObjectHeader({model: this.model}).render().el);
         this.$el.append(
-            $("<dl>")
-                .append($("<dt>").append("Checksum:"))
-                .append($("<dd>").append(this.model.get('checksum')))
-                .append($("<dt>").append("Created:"))
-                .append($("<dd>").append(this.model.get('create_time').format('lll')))
-                .append($("<dt>").append("Last Modified:"))
-                .append($("<dd>").append(this.model.get('modify_time').format('lll')))
+            $("<div>")
+                .addClass('no-preview')
+                .append("No preview available")
         );
         return this;
     }
