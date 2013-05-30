087b73942a3046f3aa104cad19704b2e26366272
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-05-29 21:57:18 -0700

    Removed database references

--- sra/__init__.py
+++ sra/__init__.py
@@ -10,9 +10,9 @@ from .models import (
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
+    #engine = engine_from_config(settings, 'sqlalchemy.')
+    #DBSession.configure(bind=engine)
+    #Base.metadata.bind = engine
     DataStoreSession.configure(
         host=settings['irods.host'],
         port=int(settings['irods.port']),
