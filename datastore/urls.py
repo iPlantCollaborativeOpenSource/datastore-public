b13dd970169a6af92f0e178360e99221fbd1ffa7
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-07-30 09:38:35 -0500

    add robots view back

--- datastore/urls.py
+++ datastore/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('sra.urls')),
 ]
