eb4e99dec870ddb81b0e6dc89f93675ded318248
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-12-03 17:10:57 -0600

    fix allowed hosts

--- datastore/settings.py
+++ datastore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '_CHANGE_ME_')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
