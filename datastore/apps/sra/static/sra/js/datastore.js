0d8567079013b17f33d517b300e8c0e40ae0d936
Marjo Poindexter <mpoindexter@dhcp-146-6-176-204.tacc.utexas.edu>
2017-01-06 17:18:15 -0600

    text changes

--- datastore/apps/sra/static/sra/js/datastore.js
+++ datastore/apps/sra/static/sra/js/datastore.js
@@ -198,18 +198,18 @@ if (!Array.prototype.map) {
 
         $scope.mouseOver = function(data) {
             if (data == 'shared') {
-                $scope.data.browseDescriptionTitle = 'Community Shared Data:';
-                $scope.data.browseDescription = "These data are provided by community collaborators for public access. Community Shared data are not curated by the Data Commons and don't have permanent identifiers. Their location and contents may change."
+                $scope.data.browseDescriptionTitle = 'Community Released:';
+                $scope.data.browseDescription = "These data are provided by community collaborators for public access. Community Released Data are not curated by the Data Commons and don't have permanent identifiers. Their location and contents may change."
 
             } else if (data == 'dcr') {
-                $scope.data.browseDescriptionTitle = 'Data Commons Repository:';
+                $scope.data.browseDescriptionTitle = 'CyVerse Curated:';
                 $scope.data.browseDescription = "All data that have been given a permanent identifier (DOI or ARK) by CyVerse. These data are stable and contents will not change.";
             } else if (data == 'ncbi') {
                 $scope.data.publishDescriptionTitle = 'NCBI-SRA:';
                 $scope.data.publishDescription = "Instructions on how to publish data to NCBI's Sequence Read Archive via the Data Commons."
             } else if (data == 'dcrPublish') {
-                $scope.data.publishDescriptionTitle = 'Data Commons Repository:';
-                $scope.data.publishDescription = "Instructions on how to request a permanent identifier and publish data to the Data Commons Repository.";
+                $scope.data.publishDescriptionTitle = 'CyVerse:';
+                $scope.data.publishDescription = "Instructions on how to request a permanent identifier and publish data to the Data Commons.";
             }
         };
 
