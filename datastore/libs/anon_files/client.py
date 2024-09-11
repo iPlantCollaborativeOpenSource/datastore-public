8b6571261ac2963abbe122f394cacb58bbaec86d
Ian McEwen <mian@cyverse.org>
2024-09-11 12:15:26 -0700

    When generating anon-files URL, remove leading slash if there is one in the file path, to prevent double slashes

--- datastore/libs/anon_files/client.py
+++ datastore/libs/anon_files/client.py
@@ -1,12 +1,12 @@
 import requests
 import settings
 import urllib
-
+import re
 
 class AnonFilesClient(object):
 
     def download_url(self, path):
-        return '{0}/{1}'.format(settings.ANON_FILES_API_HOST, urllib.quote(path))
+        return '{0}/{1}'.format(settings.ANON_FILES_API_HOST, re.sub('^/', '', urllib.quote(path)))
 
     def download(self, path, **kwargs):
         return requests.get(self.download_url(path), **kwargs)
